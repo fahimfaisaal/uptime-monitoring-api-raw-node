83a14df8123d90e109b63841201c8ea6486ceb4e
Fahim Faisal <fahimfaisal.soikot@gmail.com>
2021-07-15 23:31:21 +0600

    feat: add comments

--- handlers/routeHandler.js
+++ handlers/routeHandler.js
@@ -5,12 +5,14 @@
 */
 
 exports.rootHandler = (handleReqObj, callback) => {
+    // root handler response
     callback(200, {
         message: `It's a root route`
     })
 }
 
 exports.notFoundHandler = (handleReqObj, callback) => {
+    // not found route response
     callback(404, {
         message: `404 Invalid route: ${handleReqObj.trimmedPath}`
     })
